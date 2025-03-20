e7e56494819480d8d7bdd5501ac0cad90ad0b7bc
kekaaoweh <koweh9232@conestogac.on.ca>
2025-03-19 22:04:09 -0400

    Wrote the function that implements the unittests. Included the function in the header file used in the unittests

---
+++ CSCN71020_A3/assignment3/primenumbers.c
@@ -0,0 +1,14 @@
+#include "primenumbers.h"
+
+// function that checks if numbers are prime or not
+const char* isPrime(int num) {
+	if (num < 2) {
+		return "Not Prime";
+	}
+	for (int i = 2; i * i <= num; i++) {
+		if (num % i == 0) {
+			return "Not Prime";
+		}
+	}
+	return "Prime";
+}
